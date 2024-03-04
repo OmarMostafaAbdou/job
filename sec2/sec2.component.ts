4dcaf2b2bd51ba2631977c7a7e329b592d3b315e
omar mostafa <eng.omarmostafa22@gmail.com>
2024-03-04 15:17:20 +0200

    job

---
+++ sec2/sec2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sec2',
+
+  templateUrl: './sec2.component.html',
+  styleUrl: './sec2.component.css'
+})
+export class Sec2Component {
+
+}
---
+++ sec3/sec3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sec3',
+ 
+  templateUrl: './sec3.component.html',
+  styleUrl: './sec3.component.css'
+})
+export class Sec3Component {
+
+}
